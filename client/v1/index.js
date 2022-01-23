944c211afd0e86ed1dcc8754e5ad72dad9d6d4a9
AhmedAzough <68287399+AhmedAzough@users.noreply.github.com>
2022-01-23 10:27:12 +0100

    feat(lowest): compute number of products

--- client/v1/index.js
+++ client/v1/index.js
@@ -27,6 +27,8 @@ console.log(MY_FAVORITE_BRANDS[0]);
  */
 
 // 🎯 TODO: The cheapest t-shirt
+
+console.log("bonjour");
 // 0. I have 3 favorite brands stored in MY_FAVORITE_BRANDS variable
 // 1. Create a new variable and assign it the link of the cheapest t-shirt
 // I can find on these e-shops
